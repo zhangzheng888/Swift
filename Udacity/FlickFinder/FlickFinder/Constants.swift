0bba7fdc545296b30996bba5265cf07be5d28ed0
Kevin <zhangzheng888@gmail.com>
2018-01-20 16:32:01 -0800

    Flickr API key added

--- Udacity/FlickFinder/FlickFinder/Constants.swift
+++ Udacity/FlickFinder/FlickFinder/Constants.swift
@@ -41,7 +41,7 @@ struct Constants {
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
         static let SearchMethod = "flickr.photos.search"
-        static let APIKey = "YOUR_API_KEY_HERE"
+        static let APIKey = "fbfe8f95bf4fe42d4d41851c1160f16b"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
