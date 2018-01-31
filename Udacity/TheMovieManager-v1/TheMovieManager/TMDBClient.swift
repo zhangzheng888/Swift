ba57e6771b98aaee9879dfed68b3bba1a72867df
Kevin <zhangzheng888@gmail.com>
2018-01-30 22:56:38 -0800

    built the taskForGETMethod
    taking reused code from get methods from loginviewcontroller, set a variable parameterswithAPIKey to contain a dictionary with the API key. build the request through NSMutableRequest, changing withPathExtension to method parameter that is being passed in. the request task is similar, changing the displayError function to a sendError function. using completionHandlerForGET method to send the error back to wherever this request is called. parsing the data is handled by convertDataWithCompletionHandler closure.

--- Udacity/TheMovieManager-v1/TheMovieManager/TMDBClient.swift
+++ Udacity/TheMovieManager-v1/TheMovieManager/TMDBClient.swift
@@ -31,9 +31,51 @@ class TMDBClient : NSObject {
         super.init()
     }
 
+    // func buildTheURL()
+    // func makeTheTask()
+    // func checkForErrors()
+    
     // MARK: GET
     
-    //func taskForGETMethod(_ method: String, parameters: [String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {}
+    func taskForGETMethod(_ method: String, parameters: [String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {
+        /* 1. Set the parameters */
+        var parametersWithAPIKey = parameters
+        parametersWithAPIKey[ParameterKeys.ApiKey] = Constants.ApiKey as AnyObject?
+        /* 2/3. Build the URL, Configure the request */
+        let request = NSMutableURLRequest(url: TMDBClient.tmdbURLFromParameters(parametersWithAPIKey, withPathExtension: method))
+        /* 4. Make the request */
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForGET(nil, NSError(domain: "taskForGETMethod", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            self.convertDataWithCompletionHandler(data, completionHandlerForConvertData: completionHandlerForGET)
+        }
+        task.resume()
+        
+        return task
+    }
     
     // MARK: POST
     
