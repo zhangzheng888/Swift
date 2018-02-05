f3f02d1c1e3aed6acd883d3f8efa2eb5c228642e
Kevin <zhangzheng888@gmail.com>
2018-02-04 16:07:02 -0800

    feat: Initialized LoginViewController with IBOutlet variables

--- Udacity/OnTheGrid/OnTheGrid/LoginViewController.swift
+++ Udacity/OnTheGrid/OnTheGrid/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheGrid
 //
 //  Created by Kevin Zhang on 2/4/18.
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// MARK: - LoginViewController: UIViewController
 
+class LoginViewController: UIViewController {
+
+    // MARK: Properties
+    
+    var appDelegate: AppDelegate!
+    var keyboardOnScreen = false
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var udacityImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var debugTextLabel: UILabel!
+
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
