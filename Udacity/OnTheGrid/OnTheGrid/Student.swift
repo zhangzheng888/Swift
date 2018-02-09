653e9ef0a04192307838feccb1a9dd41829c2198
Kevin <zhangzheng888@gmail.com>
2018-02-08 23:56:03 -0800

    feat: First implementation of Student struct

---
+++ Udacity/OnTheGrid/OnTheGrid/Student.swift
@@ -0,0 +1,55 @@
+//
+//  Student.swift
+//  OnTheGrid
+//
+//  Created by Kevin Zhang on 2/8/18.
+//  Copyright © 2018 Kevin Zhang. All rights reserved.
+//
+
+import Foundation
+
+struct Student {
+    var objectId: String?
+    var uniqueKey: String?
+    var firstName: String?
+    var lastName: String?
+    var mediaURL: URL?
+    var location: Location?
+
+    init(objectId: String, uniqueKey: String?, firstName: String?, lastName: String?, mediaURL: String?, location: Location?) {
+        self.objectId = objectId
+        self.uniqueKey = uniqueKey
+        self.firstName = firstName
+        self.lastName = lastName
+        if let url = URL(string: mediaURL ?? "") {
+            self.mediaURL = url
+        } else {
+            self.mediaURL = nil
+        }
+        
+        self.location = location
+    }
+    
+    init(dictionary: [String: AnyObject], location: Location) {
+        self.objectId = getValue(from: dictionary, for: "objectId") as! String?
+        self.firstName = getValue(from: dictionary, for: "firstName") as! String?
+        self.lastName = getValue(from: dictionary, for: "lastName") as! String?
+        self.uniqueKey = getValue(from: dictionary, for: "uniqueKey") as! String?
+        if let url = URL(string: getValue(from: dictionary, for: "mediaURL") as! String? ?? "") {
+            self.mediaURL = url
+        } else {
+            self.mediaURL = nil
+        }
+        
+        self.location = location
+        
+    }
+    
+    private func getValue(from dictionary: [String: AnyObject], for key: String) -> AnyObject? {
+        if let value = dictionary[key] {
+            return value
+        } else {
+            return nil
+        }
+    }
+}
