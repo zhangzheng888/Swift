4ce7bf7595b90b55ddd85c8d5dfcd641d07b7f4b
Kevin <zhangzheng888@gmail.com>
2018-04-01 17:58:22 -0700

    feat: Custom Map Student List view cell implemented

---
+++ Udacity/OnTheGrid/OnTheGrid/MapTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MapTableViewCell.swift
+//  OnTheGrid
+//
+//  Created by Kevin Zhang on 3/31/18.
+//  Copyright © 2018 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MapTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
