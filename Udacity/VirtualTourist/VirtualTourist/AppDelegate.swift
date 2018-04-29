90340d9ccdf396ecfd0ef6688794798f4c88b0cf
Kevin <zhangzheng888@gmail.com>
2018-04-29 14:41:13 -0700

    feat: autosave added to appdelegate

--- Udacity/VirtualTourist/VirtualTourist/AppDelegate.swift
+++ Udacity/VirtualTourist/VirtualTourist/AppDelegate.swift
@@ -13,10 +13,11 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let stack = CoreDataStack(modelName: "Model")!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        stack.autoSave(60)
         return true
     }
 
