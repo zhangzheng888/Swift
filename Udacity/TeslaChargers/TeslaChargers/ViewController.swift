7c49c5b78a9782b02ad458e4f16a7225b5c78279
Kevin <zhangzheng888@gmail.com>
2018-05-26 17:31:31 -0700

    Initial project implementation

---
+++ Udacity/TeslaChargers/TeslaChargers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeslaChargers
+//
+//  Created by Kevin Zhang on 5/26/18.
+//  Copyright © 2018 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
